f93d5bda76dcc7391be92aed688533f8a828764b
Logan McGuire <lrmcguire93@gmail.com>
2020-04-24 17:30:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word (sentence)
   words = sentence.split(" ");
-  revers_words = [];
-  words.each { |word| word.reverse 
+  reverse_words = [];
+  words.each { |word| reverse_words << word.reverse };
+  
